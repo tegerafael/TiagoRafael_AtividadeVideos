1fe5f0e05672b9db6d36e6433075c67371b8f509
Tiago Rafael <tiagorafael777@gmail.com>
2018-05-02 10:35:45 -0400

    Terminado

---
+++ GrupoX/src/br/edu/ifro/AlunoController.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifro;
+
+import br.edu.ifro.modelo.Aluno;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ * FXML Controller class
+ *
+ * @author 01595747222
+ */
+public class AlunoController implements Initializable {
+
+    @FXML
+    private TextField txtNome;
+    @FXML
+    private Button btnFechar;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void salvar(ActionEvent event) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("aula");
+        EntityManager em = emf.createEntityManager();
+        
+        Aluno aluno1 = new Aluno();
+        aluno1.setNome(txtNome.getText());
+        
+        em.getTransaction().begin();
+        
+        em.persist(aluno1);
+        
+        em.getTransaction().commit();
+    }
+
+    @FXML
+    private void fechar(ActionEvent event) {
+        Stage stage = (Stage) btnFechar.getScene().getWindow();
+        stage.close();
+    }
+    
+}
--- GrupoX/src/br/edu/ifro/MenuPrincipalController.java
+++ GrupoX/src/br/edu/ifro/MenuPrincipalController.java
@@ -5,14 +5,18 @@
  */
 package br.edu.ifro;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  *
@@ -43,5 +47,25 @@ public class MenuPrincipalController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void abrirCadastroAluno(ActionEvent event) {
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(getClass().getResource("Aluno.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(),900,682);
+            Stage stage = new Stage();
+            stage.setTitle("Cadastrar Aluno");
+            stage.setScene(scene);
+            stage.show();
+}
+    catch(IOException e){
+
+}
+    }
+
+    @FXML
+    private void fecharProjeto(ActionEvent event) {
+    }
     
 }

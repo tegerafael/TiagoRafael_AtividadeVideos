f8ae1d42650af2a5b42ba971558ecdc727144855
Tiago Rafael <tiagorafael777@gmail.com>
2018-05-02 10:07:48 -0400

    Hibernate instalado

--- GrupoX/src/br/edu/ifro/MenuPrincipalController.java
+++ GrupoX/src/br/edu/ifro/MenuPrincipalController.java
@@ -10,7 +10,9 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 /**
  *
@@ -18,13 +20,23 @@ import javafx.scene.control.Label;
  */
 public class MenuPrincipalController implements Initializable {
     
-    @FXML
-    private Label label;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private TextField txtNumero1;
+    @FXML
+    private TextField txtNumero2;
+    @FXML
+    private TextField txtResultado;
+    @FXML
+    private Button btnSoma;
+    
+    @FXML
+    private void soma(ActionEvent event) {
+        Double num1 = Double.parseDouble(txtNumero1.getText());
+        Double num2 = Double.parseDouble(txtNumero2.getText());
+        Double resultado = num1 + num2;
+        
+        txtResultado.setText(resultado.toString());
     }
     
     @Override
